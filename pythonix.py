c35b5bdb1d1e6c17e20d2d452ad736ab67fd171d
Oleksii Astakhov <mydisupyshawty@Oleksiis-MacBook-Pro.local>
2024-03-20 20:50:21 +0100

    hometask

--- pythonix.py
+++ pythonix.py
@@ -1,1 +1,16 @@
-print(5 + 3)
+#1
+l_1 = list("Common Sequence Operations")
+print(".".join(l_1[6:16]))
+
+#2
+l_2 = list("Mutable Sequence Types")
+print(l_2[-5:])
+
+#3
+str = "Mutable Sequence Types"
+print(str[::2])
+
+#4
+wow = "Mutable Sequence Types"
+wowie = wow[-5:] + " " + wow[-14:-6] + " " + wow[0:-14]
+print(wowie)
